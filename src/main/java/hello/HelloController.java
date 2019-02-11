249ef0cc1f4294d58a0507bf6c0bee69e4450773
aercolak <anilcanercolak@sinerjias.com.tr>
2019-02-11 14:48:33 +0300

    v2

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Spring boot deneme projesi... (Kubernetes)";
+        return "yeni v2 spring boot deneme";
     }
     
 }
